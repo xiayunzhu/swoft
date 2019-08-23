5831e91bce50a1798b6d3522db86f0ecfaf1e45b
zhuxiayun <1083548491@qq.com>
2019-08-23 17:06:23 +0800

    datachange

---
+++ app/Migration/Goods.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+
+namespace App\Migration;
+
+
+use Swoft\Devtool\Annotation\Mapping\Migration;
+use Swoft\Devtool\Migration\Migration as BaseMigration;
+
+/**
+ * Class Goods
+ *
+ * @since 2.0
+ *
+ * @Migration(time=20190823170458)
+ */
+class Goods extends BaseMigration
+{
+    /**
+     * @return void
+     */
+    public function up(): void
+    {
+        //
+
+    }
+
+    /**
+     * @return void
+     */
+    public function down(): void
+    {
+
+    }
+}
