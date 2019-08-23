bbe931299bb1a80b0c47ad818c29a5117bbab7e3
zhuxy <1083548491@qq.com>
2019-08-23 17:11:38 +0800

    配置文件修改

--- app/Migration/Goods.php
+++ app/Migration/Goods.php
@@ -4,6 +4,10 @@
 namespace App\Migration;
 
 
+use ReflectionException;
+use Swoft\Bean\Exception\ContainerException;
+use Swoft\Db\Exception\DbException;
+use Swoft\Db\Schema\Blueprint;
 use Swoft\Devtool\Annotation\Mapping\Migration;
 use Swoft\Devtool\Migration\Migration as BaseMigration;
 
@@ -12,24 +16,38 @@ use Swoft\Devtool\Migration\Migration as BaseMigration;
  *
  * @since 2.0
  *
- * @Migration(time=20190823170458)
+ * @Migration(time=20190823170458,pool="db2.pool")
  */
 class Goods extends BaseMigration
 {
     /**
      * @return void
+     *
+     * @throws ReflectionException
+     * @throws ContainerException
+     * @throws DbException
      */
     public function up(): void
     {
         //
-
+        $this->schema->createIfNotExists('messages', function (Blueprint $blueprint) {
+            $blueprint->increments('id')->comment('商品ID');
+            $blueprint->text('content')->commnet('宝贝描述');
+            $blueprint->integer('price')->comment('价格');
+            $blueprint->string('goods_name')->comment('商品名称');
+            $blueprint->timestamps();
+        });
     }
 
     /**
      * @return void
+     *
+     * @throws ReflectionException
+     * @throws ContainerException
+     * @throws DbException
      */
     public function down(): void
     {
-
+        $this->schema->dropIfExists('messages');
     }
 }
