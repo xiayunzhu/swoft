3ff0ba1e7fb4a19c3c2d1d69a30e0bee3254200a
zhuxy <1083548491@qq.com>
2019-08-24 15:10:46 +0800

    http_server

--- app/Http/demo/Server/http_server.php
+++ app/Http/demo/Server/http_server.php
@@ -8,29 +8,16 @@
 $http = new swoole_http_server('0.0.0.0', 8811);
 $http->set([
     'enable_static_handler' => true,
-    'document_root' => '/home/vagrant/code/Lstudy/blog/resources/views'
+    'document_root' => '/home/wwwroot/swoft/app/Http/demo/Server'
 ]);
 $http->on('WorkerStart', function (swoole_server $server, $worker_id) {
     define('LARAVEL_START', microtime(true));
-
-    require __DIR__ . '/../../vendor/autoload.php';
 
 
 });
 $http->on('request', function ($request, $response) {
 
-    $app = require_once __DIR__ . '/../../bootstrap/app.php';
-    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-    $response1 = $kernel->handle(
-        $request1 = Illuminate\Http\Request::capture()
-    );
-
-    $response1->send();
-
-    $kernel->terminate($request1, $response1);
-//    $res = ob_get_contents();
-//     ob_end_clean();
     $response->end('s');
 });
 $http->start();
